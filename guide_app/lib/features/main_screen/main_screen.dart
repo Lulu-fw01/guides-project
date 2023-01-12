68bf0c017e2ec1a3d279f76b1522c9586fc0dd03
luka <limarkov@edu.hse.ru>
2023-01-13 02:16:29 +0300

    wip

--- guide_app/lib/features/main_screen/main_screen.dart
+++ guide_app/lib/features/main_screen/main_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:guide_app/common/themes/main_theme.dart';
+import 'package:provider/provider.dart';
 
 /// Main screen of the app with navigation bottom bar.
 class MainScreen extends StatefulWidget {
@@ -10,15 +12,52 @@ class MainScreen extends StatefulWidget {
 }
 
 class MainScreenState extends State<MainScreen> {
+  int selectedPageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    final theme = Provider.of<MainTheme>(context);
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home)),
-        BottomNavigationBarItem(icon: Icon(Icons.search)),
-        BottomNavigationBarItem(icon: Icon(Icons.bookmarks)),
-        BottomNavigationBarItem(icon: Icon(Icons.person))
-      ]),
+      bottomNavigationBar: NavigationBar(
+          selectedIndex: selectedPageIndex,
+          backgroundColor: theme.surface,
+          onDestinationSelected: (int index) {
+            setState(() {
+              selectedPageIndex = index;
+            });
+          },
+          destinations: [
+            NavigationDestination(
+                icon: const Icon(Icons.home_outlined),
+                selectedIcon: Icon(
+                  Icons.home,
+                  color: theme.onSurface,
+                ),
+                label: "Главное"),
+            NavigationDestination(
+                icon: const Icon(
+                  Icons.search_outlined,
+                ),
+                selectedIcon: Icon(
+                  Icons.search,
+                  color: theme.onSurface,
+                ),
+                label: "Поиск"),
+            NavigationDestination(
+                icon: const Icon(Icons.bookmarks_outlined),
+                selectedIcon: Icon(
+                  Icons.bookmarks,
+                  color: theme.onSurface,
+                ),
+                label: "Избранное"),
+            NavigationDestination(
+                icon: const Icon(Icons.person_outline),
+                selectedIcon: Icon(
+                  Icons.person,
+                  color: theme.onSurface,
+                ),
+                label: "Профиль")
+          ]),
     );
   }
 }
