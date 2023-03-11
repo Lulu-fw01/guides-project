1412dc0ad804191712cc23100f4a576608c908e1
luka <limarkov@edu.hse.ru>
2023-03-12 01:58:39 +0300

    wip

--- guide_app/lib/features/profile/cubit/profile_cubit.dart
+++ guide_app/lib/features/profile/cubit/profile_cubit.dart
@@ -1,8 +1,14 @@
 import 'package:bloc/bloc.dart';
+import 'package:guide_app/common/repository/guide/i_guide_repository.dart';
 import 'package:meta/meta.dart';
 
 part 'profile_state.dart';
 
 class ProfileCubit extends Cubit<ProfileState> {
-  ProfileCubit() : super(ProfileInitial());
+  ProfileCubit(this.guideRepository) : super(ProfileInitial());
+  final IGuideRepository guideRepository;
+
+  void getNextPage(String email, int pageNum) {
+    emit(ProfileLoadingState());
+  }
 }
--- guide_app/lib/features/profile/screens/profile_screen.dart
+++ guide_app/lib/features/profile/screens/profile_screen.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:guide_app/common/client/guide_client.dart';
+import 'package:guide_app/common/repository/guide/guide_repository.dart';
 import 'package:guide_app/common/themes/main_theme.dart';
 import 'package:guide_app/cubit/init_cubit.dart';
 import 'package:guide_app/features/profile/cubit/profile_cubit.dart';
 import 'package:guide_app/features/profile/widgets/profile_content.dart';
 import 'package:provider/provider.dart';
 
-class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({super.key});
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key, required this.email, required this.token});
+  final String email;
+  final String token;
 
-  @override
-  ProfileScreenState createState() => ProfileScreenState();
-}
-
-class ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     final initCubit = Provider.of<InitCubit>(context);
     final theme = Provider.of<MainTheme>(context);
     return BlocProvider(
-        create: (context) => ProfileCubit(), child: ProfileContent());
+        // TODO move repository to provider.
+        create: (context) =>
+            ProfileCubit(GuideRepository(email, GuideClient(token))),
+        child: ProfileContent());
   }
 }
