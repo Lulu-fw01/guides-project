88da47ff54e551e4debd8d2176439b4f671c7e57
Федор Филиппов <fefilippov@edu.hse.ru>
2023-04-04 23:08:57 +0300

    fixed email-login issue

--- serverApp/src/main/java/com/server/repository/FavoriteItemRepository.java
+++ serverApp/src/main/java/com/server/repository/FavoriteItemRepository.java
@@ -1,5 +1,11 @@
 package com.server.repository;
 
+import org.hibernate.Session;
+import org.hibernate.query.NativeQuery;
+import org.hibernate.type.BooleanType;
+import org.hibernate.type.LongType;
+import org.hibernate.type.StringType;
+import org.hibernate.type.TimestampType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,8 +21,43 @@ public interface FavoriteItemRepository extends JpaRepository<FavoriteItem, Favo
 
     @Query(value =
             "SELECT guide_id FROM favorites f WHERE f.user_email IN " +
-            "(SELECT email FROM users u WHERE u.email = :email) ORDER BY add_date_time DESC",
+                    "(SELECT email FROM users u WHERE u.email = :email) ORDER BY add_date_time DESC",
             nativeQuery = true
     )
     List<Long> findFavoritesByConcreteUser(@Param("email") String email);
+
+    default NativeQuery<Object[]> getPagesByCursor(Session session, Integer pageSize, Integer cursor, String email) {
+        return session.createNativeQuery(
+                        "SELECT id, creator_email, title, file_bytes, edit_date, is_blocked " +
+                                "FROM guides " +
+                                "JOIN (SELECT * FROM favorites f WHERE f.user_email IN (SELECT email FROM users u WHERE u.email = '" + email + "')) as ord " +
+                                "ON ord.guide_id = guides.id " +
+                                "WHERE (SELECT add_date_time FROM favorites WHERE guide_id = " + cursor + " AND user_email = '" + email + "')" + " > " + "ord.add_date_time " +
+                                "ORDER BY add_date_time DESC " +
+                                "LIMIT " + pageSize
+                )
+                .addScalar("id", LongType.INSTANCE)
+                .addScalar("creator_email", StringType.INSTANCE)
+                .addScalar("title", StringType.INSTANCE)
+                .addScalar("file_bytes", StringType.INSTANCE)
+                .addScalar("edit_date", TimestampType.INSTANCE)
+                .addScalar("is_blocked", BooleanType.INSTANCE);
+    }
+
+    default NativeQuery<Object[]> getAllPages(Session session, Integer pageSize, String email) {
+        return session.createNativeQuery(
+                        "SELECT id, creator_email, title, file_bytes, edit_date, is_blocked " +
+                                "FROM guides " +
+                                "JOIN (SELECT * FROM favorites f WHERE f.user_email IN (SELECT email FROM users u WHERE u.email = '" + email + "')) as ord " +
+                                "ON ord.guide_id = guides.id " +
+                                "ORDER BY add_date_time DESC " +
+                                "LIMIT " + pageSize
+                )
+                .addScalar("id", LongType.INSTANCE)
+                .addScalar("creator_email", StringType.INSTANCE)
+                .addScalar("title", StringType.INSTANCE)
+                .addScalar("file_bytes", StringType.INSTANCE)
+                .addScalar("edit_date", TimestampType.INSTANCE)
+                .addScalar("is_blocked", BooleanType.INSTANCE);
+    }
 }
